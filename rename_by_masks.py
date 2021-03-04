fb963b6f56ab838fba1fd3ba08d85a915071a80f
maninserg <manin.serg@gmail.com>
2021-03-04 21:43:44 +0300

    Add rename_by_masks.py

---
+++ rename_by_masks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.6
+import os
+"""
+The script replace MASK_DEL to MASK_PS in names of files in the current directory
+Write MASK_DEL and MASK_PS in SETTINGS
+"""
+
+
+# SETTINGS
+MASK_DEL ="_Программирование_на_Python"
+MASK_PS = ""
+# SETTINGS
+
+list_name_all = os.listdir(path=".")
+
+for item in list_name_all:
+    os.replace(item,item.replace(MASK_DEL,MASK_PS))
+
+
+
